0e1bf435bbb03fc7488ed767088a5bf3e181cf57
AleixTomas-ITB2425 <aleix.tomas.7e8@itb.cat>
2024-09-27 09:14:26 +0200

    Text

--- TAO2/Edat.py
+++ TAO2/Edat.py
@@ -1,7 +1,7 @@
 edat=int(input("Quina edat tens?"))
 if edat > 18:
     print("Ets major d'edat")
-elif edat < 18:
-    print("No ets major d'edat")
+else:
+    print("Ets menor d'edat")
 
 print("Programa finalizat")
